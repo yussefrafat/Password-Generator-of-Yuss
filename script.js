84d8f4af400cb40f716c97fab5be3499c134e249
Yussef Rafat <yussefrafat5908@gmail.com>
2021-06-13 12:28:29 -0400

    added the value of var

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
+
 // Assignment code here
-window.alert("")
+var enter; 
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+number = [0,1,2,3,4,5,6,7,8,9];
+characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// for the Uppercase conversion !!!!!
+space = [];
+// Choices declared after if statement so they can be concatenated upon condition
+var choices;
+// converts letters to uppercase
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
+letters2 = letters.map(toUpper);
+
 // Get references to the #generate element
-var button = document.querySelector("#generate");
-var password = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-let characters = "0123456789ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz!@#$%^&*()_-+={}[]<>?";
-let passwordLenght = 10;
-let passwordValue = ""
+function writePassword() {
+  window.prompt("How many characters would you like to have on your password? Between 8 and 128")
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-const createPassowrd = () => {
-    passwordValue = "";
+  passwordText.value = password;
 
-    for(let i = 0; i < passwordLenght; i++) {
-        let number = Math.floor(Math.random() * characters.length);
-        passwordValue += characters.substring(number,number + 1);
-    }
-
-    password.value = passwordValue;
 }
 
 // Add event listener to generate button
-button.addEventListener("click", createPassowrd);
+generateBtn.addEventListener("click", writePassword);
