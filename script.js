2ec99e0073a9b2a1718e1d6b8a69b1100c76c551
Yussef Rafat <yussefrafat5908@gmail.com>
2021-06-12 22:32:38 -0400

    added a random generator

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // Assignment code here
-
-
+window.alert("")
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var button = document.querySelector("#generate");
+var password = document.querySelector("#password");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+let characters = "0123456789ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz!@#$%^&*()_-+={}[]<>?";
+let passwordLenght = 10;
+let passwordValue = ""
 
-  passwordText.value = password;
+const createPassowrd = () => {
+    passwordValue = "";
 
+    for(let i = 0; i < passwordLenght; i++) {
+        let number = Math.floor(Math.random() * characters.length);
+        passwordValue += characters.substring(number,number + 1);
+    }
+
+    password.value = passwordValue;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+button.addEventListener("click", createPassowrd);
